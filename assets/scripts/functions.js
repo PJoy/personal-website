57725be5dc1eece1c50495a4c680d3936bc90689
Pierre Portejoie <portejoiepierre@gmail.com>
2016-06-20 19:16:00 +0200

    almost there !

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -4,9 +4,27 @@
 
 $(document).ready(function(){
     applyHeader();
+    colorSkills('#svg1', 8, '#E84A5F', '#2A363B');
+    colorSkills('#svg2', 8, '#E84A5F', '#2A363B');
+    colorSkills('#svg3', 3, '#E84A5F', '#2A363B');
+
 });
 
 function applyHeader()
 {
     $('header').css({ height: ($(window).height()) +'px' });
+}
+
+function colorSkills(elemt, grade, col1, col2) {
+    var gElems = [$(elemt +' path:nth-of-type(3)'), $(elemt +' path:nth-of-type(7)'), $(elemt +' path:nth-of-type(5)'), $(elemt +' path:nth-of-type(8)'), $(elemt +' path:nth-of-type(6)'), $(elemt +' path:nth-of-type(4)'), $(elemt +' path:nth-of-type(2)'), $(elemt +' path:nth-of-type(1)')];
+
+    for (var i = 0; i < 8; i++){
+        if ((i) < grade ) gElems[i].attr("fill", col1);
+        else gElems[i].attr("fill", col2)
+    }
+}
+
+function showHideProjects (button, type){
+    $(button).toggleClass('clicked');
+    $('.'+type).fadeToggle();
 }
